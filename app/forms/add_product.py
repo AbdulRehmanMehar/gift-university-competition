43379e2f6c43a5e3f9f578464a3080ef48440230
Abdul Rehman <mehars.6925@gmail.com>
2019-05-11 21:45:48 +0500

    Add product validation form

---
+++ app/forms/add_product.py
@@ -0,0 +1,26 @@
+from wtforms import Form, IntegerField, TextAreaField, StringField, validators
+from ..models import Category
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+
+
+def get_cats():
+    return Category.query
+
+
+class AddProductForm(Form):
+
+    title = StringField('Name', [
+        validators.DataRequired(),
+        validators.length(min=5, max=50)
+    ], description='Surgical Knife')
+
+    price = IntegerField('Price', [
+        validators.DataRequired(),
+    ], description='150 pkr')
+
+    description = TextAreaField('Description', [
+        validators.DataRequired(),
+        validators.length(min=100, max=1500)
+    ], description='Anything about the product')
+
+    category = QuerySelectField('Category', query_factory=get_cats, get_pk=lambda a: a.id, get_label=lambda a: a.name, allow_blank=False)
