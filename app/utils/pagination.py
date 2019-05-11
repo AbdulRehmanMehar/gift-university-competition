2640b337832f64cd0030431eebab6d8da3e065dd
Abdul Rehman <mehars.6925@gmail.com>
2019-05-11 21:53:57 +0500

    a utility to paginate

---
+++ app/utils/pagination.py
@@ -0,0 +1,48 @@
+from math import ceil
+from sqlalchemy.orm.collections import InstrumentedList
+
+
+class Pagination:
+
+    def __init__(self, model, items_per_page, current_page_number):
+        if type(model) is InstrumentedList:
+            self.list = model
+            self.total_pages = int(ceil(len(self.list) / items_per_page))
+        else:
+            self.model = model
+            self.total_pages = int(ceil(model.query.count() / items_per_page))
+
+        self.items_per_page = items_per_page
+        self.current_page_number = current_page_number
+
+    @property
+    def pages(self):
+        return self.total_pages
+
+    def get_items_per_page(self):
+        return self.items_per_page
+
+    def get_current_page_number(self):
+        return self.current_page_number
+
+    def get_results(self):
+
+        if self.list:
+            st_idx = (self.current_page_number - 1) * self.items_per_page
+            end_idx = self.current_page_number * self.items_per_page
+            return self.list[st_idx: end_idx]
+        else:
+            if self.current_page_number == 1:
+                return self.model.query\
+                        .limit(self.items_per_page)\
+                        .all()
+
+            elif self.current_page_number <= self.total_pages:
+
+                return self.model.query\
+                        .limit(self.items_per_page)\
+                        .offset((self.current_page_number - 1) * self.items_per_page)\
+                        .all()
+
+            return None
+
