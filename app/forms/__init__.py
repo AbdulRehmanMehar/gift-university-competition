6380ec6d5ef34c961057402b00a405a31a60a5db
Abdul Rehman <mehars.6925@gmail.com>
2019-05-11 21:41:48 +0500

    forms for validation

---
+++ app/forms/__init__.py
@@ -0,0 +1,10 @@
+from .. import app
+from .login import LoginForm
+from .registeration import RegisterationForm
+from .photo import PhotoUploadForm
+from .update_profile import EditProfileForm
+from .add_category import AddCategoryForm
+from .update_category import UpdateCategoryForm
+from .add_product import AddProductForm
+from .update_product import UpdateProductForm
+
