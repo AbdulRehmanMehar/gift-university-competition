c0bf6b5e340ab99808b62edfc26ffb882eb232c7
Abdul Rehman <mehars.6925@gmail.com>
2019-05-19 14:16:59 +0500

    ajax requests to update, delete or fetch cart

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    let app_uri = $('meta[name="base_uri"]').attr('content');
     let href = window.location.href.split('/').pop();
     $('.nav-link').each((i, el) => {
         let hr = $(el).attr('href').split('/').pop();
@@ -31,19 +32,73 @@ $(document).ready(() => {
         });
     });
 
+    $('.cart-update-form').each((i, el) => {
+        $(el).submit(event => {
+            event.preventDefault();
+            let qtty = $(event.target).find($('input[type=number]')).val();
+            let pslug = $(event.target).find($('input[type=hidden]')).val();
+            updateTheCart(qtty, pslug);
+        });
+    });
+
+    $('.delete-cart-item').each((i, el) => {
+        $(el).click((event) => {
+            event.preventDefault();
+            let pslug = $(event.target).find($('.sr-only')).html();
+            $(`#tr-${pslug}`).remove();
+            deleteFromCart(pslug);
+        });
+    });
+
     function addToCart(qtty, pslug) {
         let cartItems = $('#cart_items');
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:5000/add-to-cart/' + pslug,
+            url: `${app_uri}/add-to-cart/${pslug}`,
             data: {
                 quantity: qtty
             },
-            success: (data) => {
-                let n = parseInt($(cartItems).html()) + parseInt(qtty);
-                $(cartItems).html(n);
+            success: () => updateCartLength(),
+            error: (msg) => console.error(msg)
+        });
+    }
+
+
+    function updateTheCart(qtty, pslug) {
+        $.ajax({
+            type: 'POST',
+            url: `${app_uri}/update-cart/${pslug}`,
+            data: {
+                quantity: qtty
+            },
+            success: () => {
+                updateCartLength();
+                let price = parseInt($(`#${pslug}-price`).html());
+                let total = price * qtty;
+                $(`#${pslug}-qtty`).html(qtty);
+                $(`#${pslug}-total`).html(total);
             },
             error: (msg) => console.error(msg)
+        })
+    }
+
+    function deleteFromCart(pslug) {
+        $.ajax({
+            type: 'POST', 
+            url: `${app_uri}/remove-from-cart/${pslug}`,
+            success: () => updateCartLength(),
+            error: (msg) => console.error(msg)
+        })
+    }
+
+    function updateCartLength() {
+        let cartItems = $('#cart_items');
+        $.ajax({
+            type: 'POST',
+            url: `${app_uri}/cart-len`,
+            success: (len) => {
+                $(cartItems).html(len);
+            }
         });
     }
 
