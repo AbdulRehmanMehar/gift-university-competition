55bbf44babf6478704d846067c91fbcd980a88c6
Abdul Rehman <mehars.6925@gmail.com>
2019-05-19 14:16:35 +0500

    updates and deletes the cart

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -10,8 +10,10 @@ index = Blueprint('app', __name__)
 @app.context_processor
 def inject():
     cat = Category.query.all()
-    # session['cart'] = {}
-    return dict(categories=cat, cart=session['cart'])
+    # session['cart'] = { }
+    cart = session.get('cart') if session.get('cart') else {'len': 0}
+    # print(cart)
+    return dict(categories=cat, cart=cart)
 
 
 @index.route('/')
@@ -22,6 +24,11 @@ def home():
 @index.route('/cart')
 def cart():
     return render_template('cart.html')
+
+
+@index.route('/cart-len', methods=['POST'])
+def cart_len():
+    return str(session['cart']['len']) if session.get('cart') else str(0)
 
 
 @index.route('/add-to-cart/<string:slug>', methods=['POST'])
@@ -51,14 +58,46 @@ def add_to_cart(slug):
             length = d.get('quantity')
             try:
                 length += session['cart']['len']
+                session['cart'] = {
+                    **session['cart'],
+                    f'p-{prod.id}': d,
+                    'len': length
+                }
             except KeyError:
                 length = d.get('quantity')
-            session['cart'] = {
-                **session['cart'],
-                f'p-{prod.id}': d,
-                'len': length
-            }
+                session['cart'] = {
+                    f'p-{prod.id}': d,
+                    'len': length
+                }
+           
         return f'{slug}, {request.form["quantity"]} The Product Id'
+    return abort(500)
+
+
+@index.route('/update-cart/<string:slug>', methods=['POST'])
+def update_cart(slug):
+    qtty = int(request.form['quantity'])
+    prod = Product.query.filter(Product.slug == slug).first()
+    if prod and session.get('cart'):
+        length = session['cart']['len'] - \
+            session['cart'][f'p-{prod.id}']['quantity']
+        session['cart']['len'] = length + qtty
+        session['cart'][f'p-{prod.id}']['quantity'] = qtty
+        session['cart'] = session['cart']
+        return 'all is well'
+    return abort(500)
+
+
+@index.route('/remove-from-cart/<string:slug>', methods=['POST'])
+def remove_cart(slug):
+    prod = Product.query.filter(Product.slug == slug).first()
+    if prod and session.get('cart'):
+        length = session['cart']['len'] - \
+            session['cart'][f'p-{prod.id}']['quantity']
+        session['cart']['len'] = length
+        del session['cart'][f'p-{prod.id}']
+        session['cart'] = session['cart']
+        return 'all is well'
     return abort(500)
 
 
