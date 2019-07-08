4b3b82423b0b2626d455bb5c36a7056d8a720fec
Abdul Rehman <mehars.6925@gmail.com>
2019-07-08 20:16:00 +0500

    updated to store owner and manufacturer's info

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,5 @@
 from . import db
+from datetime import datetime
 
 
 class Product(db.Model):
@@ -12,16 +13,26 @@ class Product(db.Model):
     featured = db.Column(db.Boolean, default=False)
     description = db.Column(db.String(1500), nullable=False)
     photo = db.Column(db.String, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-    category = db.relationship('Category', backref=db.backref('products', uselist=True))
+    manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturers.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    owner = db.relationship(
+        'User', backref=db.backref('products', uselist=True))
+    category = db.relationship(
+        'Category', backref=db.backref('products', uselist=True))
+    manufacturer = db.relationship(
+        'Manufactures', backref=db.backref('products', uselist=True))
 
-    def __init__(self, title, price, quantity, featured,description, photo, category):
+    def __init__(self, title, price, quantity, featured, description, photo, owner, category, manufactures):
         self.title = title
         self.price = price
         self.photo = photo
+        self.owner_id = owner
         self.quantity = quantity
         self.featured = featured
         self.category_id = category
+        self.manufactures_id = manufactures
         self.description = description
         self.slug = self.set_slug(title)
 
