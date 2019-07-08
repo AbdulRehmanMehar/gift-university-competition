895bdd6f090652333c5039fd23877743d01c5613
Abdul Rehman <mehars.6925@gmail.com>
2019-07-08 21:10:12 +0500

    address book routes

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -1,6 +1,6 @@
 import yaml
-from ..utils import Cart
-from ..models import User, Order
+from ..utils import Cart, Pagination
+from ..models import db, User, Order, AddressBook
 from flask_login import current_user
 from flask import Blueprint, render_template, redirect, url_for, request, abort, flash, Markup
 
@@ -49,3 +49,56 @@ def view_order(id):
     if res:
         return render_template('dashboard/orders/viewable.html', crt=yaml.load(res.cart), order=res)
     return redirect(url_for('dashboard.order'))
+
+
+@dashboard.route('/address-book')
+def address_book():
+    return render_template('dashboard/addressbook/home.html')
+
+
+@dashboard.route('/address-book/add', methods=['GET', 'POST'])
+def add_address():
+
+    if request.method == 'POST':
+        name = request.form.get('name').strip()
+        address = request.form.get('address').strip()
+        if name and address:
+            book = AddressBook(name, address, current_user.id)
+            db.session.add(book)
+            db.session.commit()
+        return redirect(url_for('dashboard.address_book'))
+
+    return render_template('dashboard/addressbook/home.html')
+
+
+@dashboard.route('/address-book/edit/<int:id>', methods=['GET', 'POST'])
+def update_address(id):
+    address = AddressBook.query.filter(AddressBook.id == id).first()
+    if request.method == 'POST':
+        address.name = request.form.get('name').strip()
+        address.address = request.form.get('address').strip()
+        db.session.commit()
+        return redirect(url_for('dashboard.address_book'))
+
+    return render_template('dashboard/addressbook/home.html', address=address)
+
+
+@dashboard.route('/address-book/delete/<int:id>')
+def delete_address(id):
+    address = AddressBook.query.filter(AddressBook.id == id).first()
+    db.session.delete(address)
+    db.session.commit()
+    redir = request.args.get('next') or request.referrer or url_for('app.home')
+    return redirect(redir)
+
+
+@dashboard.route('/address-book/view', defaults={'page': 1})
+@dashboard.route('/address-book/view/<int:page>')
+def view_address_book(page):
+    paginator = Pagination(AddressBook, 10, page)
+    return render_template('dashboard/addressbook/view.html',
+                           pages=paginator.pages,
+                           addresses=paginator.get_results(),
+                           items_per_page=paginator.get_items_per_page(),
+                           current_page_number=paginator.get_current_page_number()
+                           )
