5a45bd4cd0c7cbd80ba05e93d8415c09e8338547
Abdul Rehman <mehars.6925@gmail.com>
2019-07-08 20:15:05 +0500

    routes for manufacture crud

--- app/controllers/admin.py
+++ app/controllers/admin.py
@@ -3,7 +3,7 @@ from .. import app
 from flask import abort, flash, Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user
 from ..forms import AddCategoryForm, UpdateCategoryForm, AddProductForm, UpdateProductForm, PhotoUploadForm
-from ..models import db, Category, Product, Order
+from ..models import db, Category, Product, Order, Manufactures
 from ..utils import Pagination
 
 admin = Blueprint('admin', __name__)
@@ -81,6 +81,56 @@ def view_categories(page):
                            )
 
 
+# Manufacturer Routes
+
+
+@admin.route('/manufacturers')
+def manufacturers():
+    return render_template('admin/manufacturers/home.html')
+
+
+@admin.route('/manufacturers/add', methods=['GET', 'POST'])
+def add_manufacturers():
+    if request.method == 'POST' and request.form.get('name').strip():
+        man = Manufactures(request.form.get('name'))
+        db.session.add(man)
+        db.session.commit()
+        return redirect(url_for('admin.view_manufacturers'))
+
+    return render_template('admin/manufacturers/add.html')
+
+
+@admin.route('/manufacturers/delete/<int:id>')
+def delete_manufacturers(id):
+    cat = Manufactures.query.filter(Manufactures.id == id).first()
+    db.session.delete(cat)
+    db.session.commit()
+    return redirect(url_for('admin.view_manufacturers'))
+
+
+@admin.route('/manufacturers/edit/<int:id>', methods=['GET', 'POST'])
+def update_manufacturers(id):
+    man = Manufactures.query.filter(Manufactures.id == id).first()
+    if request.method == 'POST' and request.form.get('name').strip():
+        man.name = request.form.get('name')
+        # man.slug = man.set_slug(man.name)
+        db.session.commit()
+        return redirect(url_for('admin.view_manufacturers'))
+    return render_template('admin/manufacturers/edit.html', man=man)
+
+
+@admin.route('/manufacturers/view', defaults={'page': 1})
+@admin.route('/manufacturers/view/<int:page>')
+def view_manufacturers(page):
+    paginator = Pagination(Manufactures, 10, page)
+    return render_template('admin/manufacturers/view.html',
+                           pages=paginator.pages,
+                           manufacturers=paginator.get_results(),
+                           items_per_page=paginator.get_items_per_page(),
+                           current_page_number=paginator.get_current_page_number()
+                           )
+
+
 # Product Routes
 
 
@@ -94,9 +144,13 @@ def add_product():
     form = AddProductForm(request.form)
     photo_form = PhotoUploadForm(request.files)
     if request.method == 'POST' and form.validate() and photo_form.validate():
-        cat = Category.query.filter(Category.name == str(form.category.data)).first()
+        cat = Category.query.filter(
+            Category.name == str(form.category.data)).first()
+        man = Manufactures.query.filter(
+            Manufactures.name == str(form.manufacturer.data)).first()
         file = f'{form.title.data}.{photo_form.ext}'
-        prod = Product(form.title.data, form.price.data, form.quantity.data, form.featured.data == 'True', form.description.data, file, cat.id)
+        prod = Product(form.title.data, form.price.data, form.quantity.data,
+                       form.featured.data == 'True', form.description.data, file, current_user.id, cat.id, man)
         photo = request.files[photo_form.photo.name]
         photo.save(os.path.join(f'{app.config["UPLOADS_FOLDER"]}/products', file))
         db.session.add(prod)
@@ -130,10 +184,14 @@ def update_product(id):
             photo = request.files[photo_form.photo.name]
             os.remove(os.path.join(f'{app.config["UPLOADS_FOLDER"]}/products', prod.photo))
             photo.save(os.path.join(f'{app.config["UPLOADS_FOLDER"]}/products', file))
-        cat = Category.query.filter(Category.name == str(form.category.data)).first()
+        cat = Category.query.filter(
+            Category.name == str(form.category.data)).first()
+        man = Manufactures.query.filter(
+            Manufactures.name == str(form.manufacturer.data)).first()
         prod.title = form.title.data
         prod.price = form.price.data
         prod.category_id = cat.id
+        prod.manufacturer_id = man.id
         prod.quantity = form.quantity.data
         prod.featured = form.featured.data == 'True'
         prod.photo = file
